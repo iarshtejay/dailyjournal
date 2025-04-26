6be2eab7ee2383c73ae951b967c7e8d8c2050506
iarshtejay <arshtejay@gmail.com>
2025-04-26 19:44:54 -0300

    chore: remove db console.log in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,28 @@
-require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` });
+require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` });
 const express = require("express");
 const app = express();
 const routes = require("./src/routes");
 const bodyParser = require("body-parser");
 const dbConnnection = require("./src/db/connection");
-const cors = require('cors');
-const port = process.env.PORT || 3002
+const cors = require("cors");
+const port = process.env.PORT || 3002;
 
-console.log(process.env.DATABASE_URI)
+//console.log(process.env.DATABASE_URI);
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/images', express.static('./images'))
+app.use("/images", express.static("./images"));
 // app.use(cors({
 //   origin: 'http://localhost:3000'
 // }));
-app.use(cors())
+app.use(cors());
 
 app.use("/api", routes);
 app.get("*", function (req, res) {
   res.status(404).send({
-      message: "No such route found.",
+    message: "No such route found.",
   });
 });
-
 
 dbConnnection.on("connected", () => {
   console.log("Database connected");
@@ -32,4 +31,4 @@ dbConnnection.on("connected", () => {
   });
 });
 
-module.exports = app
+module.exports = app;
